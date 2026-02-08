514de02b831685ad88aa2295238dcf77f3237a37
murpheus007 <dreamgotwings@gmail.com>
2026-02-08 17:26:43 +0100

    deploy error

--- src/components/trades/TradeDetailsDrawer.tsx
+++ src/components/trades/TradeDetailsDrawer.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import { Drawer } from "../ui/Drawer";
 import { Button } from "../ui/Button";
 import { Input } from "../ui/Input";
